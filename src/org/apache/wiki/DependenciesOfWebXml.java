fa2f3f8ec0486b43f4ab182e759695e1220311a9
PuppyPi <puppyofcodez@gmail.com>
2022-06-16 02:17:02 +0000

    Begin semantically tagging dependencieeeeeees! :D

---
+++ src/org/apache/wiki/DependenciesOfWebXml.java
@@ -0,0 +1,21 @@
+package org.apache.wiki;
+
+import rebound.annotations.semantic.meta.dependencies.DependencyClass;
+
+@DependencyClass(org.apache.wiki.WikiServlet.class)
+@DependencyClass(org.apache.wiki.ajax.WikiAjaxDispatcherServlet.class)
+@DependencyClass(org.apache.wiki.attachment.AttachmentServlet.class)
+@DependencyClass(org.apache.wiki.auth.SessionMonitor.class)
+@DependencyClass(org.apache.wiki.ui.WikiJSPFilter.class)
+@DependencyClass(org.apache.wiki.ui.WikiServletFilter.class)
+
+//@DependencyClass(org.apache.wiki.bootstrap.WikiBootstrapServletContextListener.class)
+
+//@DependencyClass(org.apache.wiki.rpc.atom.AtomAPIServlet.class)
+//@DependencyClass(org.apache.wiki.xmlrpc.RPCHandler.class)
+//@DependencyClass(org.apache.wiki.xmlrpc.RPCHandlerUTF8.class)
+//@DependencyClass(org.apache.wiki.xmlrpc.RPCServlet.class)
+
+public class DependenciesOfWebXml
+{
+}
---
+++ src/org/apache/wiki/ToplevelDependencyAnchorForManyWiki.java
@@ -0,0 +1,9 @@
+package org.apache.wiki;
+
+import rebound.annotations.semantic.meta.dependencies.DependencyClass;
+
+@DependencyClass(DependenciesOfWebXml.class)
+@DependencyClass(DependenciesOfJSPTagLibrary.class)
+public class ToplevelDependencyAnchorForManyWiki
+{
+}
