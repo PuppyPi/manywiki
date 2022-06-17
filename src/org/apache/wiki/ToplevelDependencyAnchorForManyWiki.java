045d61289344eda969b35fd292c994f93f433864
PuppyPi <puppyofcodez@gmail.com>
2022-06-17 05:05:10 +0000

    Add JSTL!! Apparently that's bundled with all webapps! ..I love that!! :D (Then everyone can be different and use whatever they want :'> )

--- src/org/apache/wiki/ToplevelDependencyAnchorForManyWiki.java
+++ src/org/apache/wiki/ToplevelDependencyAnchorForManyWiki.java
@@ -1,12 +1,15 @@
 package org.apache.wiki;
 
+import org.apache.taglibs.standard.ToplevelDependenciesAnchorForJSTLImplementation;
 import org.apache.wiki.render.DefaultRenderingManager;
 import rebound.annotations.semantic.meta.dependencies.DependencyClass;
 
+@DependencyClass(ToplevelDependenciesAnchorForJSTLImplementation.class)
 @DependencyClass(DependenciesOfWebXml.class)
 @DependencyClass(DependenciesOfJSPTagLibrary.class)
 @DependencyClass(DependenciesOfJSPWikiProperties.class)
 @DependencyClass(DefaultRenderingManager.class)  //TODO put this in the "proper" configuration system when that's done!
+
 public class ToplevelDependencyAnchorForManyWiki
 {
 }
