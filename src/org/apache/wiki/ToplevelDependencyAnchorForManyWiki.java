677cc71c6c8b779f6ced64449f5b311d963c826a
PuppyPi <puppyofcodez@gmail.com>
2022-06-17 01:38:41 +0000

    Fixing dependencies that must be in this project, not the core!

--- src/org/apache/wiki/ToplevelDependencyAnchorForManyWiki.java
+++ src/org/apache/wiki/ToplevelDependencyAnchorForManyWiki.java
@@ -5,6 +5,7 @@ import rebound.annotations.semantic.meta.dependencies.DependencyClass;
 @DependencyClass(DependenciesOfWebXml.class)
 @DependencyClass(DependenciesOfJSPTagLibrary.class)
 @DependencyClass(DependenciesOfJSPWikiProperties.class)
+@DependencyClass(org.apache.wiki.render.DefaultRenderingManager.class)  //TODO put this in the "proper" configuration system when that's done!
 public class ToplevelDependencyAnchorForManyWiki
 {
 }
