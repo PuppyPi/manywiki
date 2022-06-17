aeb3b40c5bfa157034abd402031d8bd54fe4d613
PuppyPi <puppyofcodez@gmail.com>
2022-06-17 03:16:55 +0000

    Include jspwiki.properties in the jar

--- src/org/apache/wiki/ToplevelDependencyAnchorForManyWiki.java
+++ src/org/apache/wiki/ToplevelDependencyAnchorForManyWiki.java
@@ -1,11 +1,13 @@
 package org.apache.wiki;
 
 import rebound.annotations.semantic.meta.dependencies.DependencyClass;
+import rebound.annotations.semantic.meta.dependencies.DependencyFile;
 
 @DependencyClass(DependenciesOfWebXml.class)
 @DependencyClass(DependenciesOfJSPTagLibrary.class)
 @DependencyClass(DependenciesOfJSPWikiProperties.class)
 @DependencyClass(org.apache.wiki.render.DefaultRenderingManager.class)  //TODO put this in the "proper" configuration system when that's done!
+@DependencyFile("jspwiki.properties")
 public class ToplevelDependencyAnchorForManyWiki
 {
 }
